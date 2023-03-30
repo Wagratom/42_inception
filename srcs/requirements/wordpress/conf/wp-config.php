d5c3942fb5f56adc6d8eef12d1445c19d0a79fe0
wallas <wagratom22@gmail.com>
2023-03-30 15:10:50 -0300

    create .env

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'www-data' );
 define( 'DB_PASSWORD', 'EscravoDa42' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'mariadb' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
