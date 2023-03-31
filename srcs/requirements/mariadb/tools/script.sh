baf176736bb7bd1b73513e5c595a32161d95f7b0
wallas <wagratom22@gmail.com>
2023-03-31 13:15:05 -0300

    indo scpt to add users

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+#Iniciando o servido mysql
 service mysql start
+
+# Criando uma nova base de dados em pt br
 mysql -e "CREATE DATABASE $MYSQL_DATABASE CHARACTER SET utf8 COLLATE utf8_general_ci"
+
+# Criando um user que nao vai ser local '%' e um password
 mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"
+
+# Dando todos os privilegios para o usuario e para data de base
 mysql -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%'"
 
+# Alterando a senha root
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'"
+# Dando um update
 mysql -e "flush privileges"
 
+#iniciando o serviço em pid 1
 mysqld
---
+++ srcs/requirements/wordpress/script.sh
@@ -0,0 +1,3 @@
+wp user create wwallas wwallas@beldade.com --role=administrator --user_pass=123 --allow-root --path=/var/www/html/wordpress
+wp user create joao joao@pedefeijao.com --role=author --user_pass=123 --allow-root --path=/var/www/html/wordpress
+php-fpm7.3, -F
